f2222b1dc56774a8b97067854e1be40045d2f94d
Felix Marti <martivalverde.98@gmail.com>
2021-06-29 13:52:06 +0200

    keyboard controller changed

--- sim/scripts/keyboard_control.py
+++ sim/scripts/keyboard_control.py
@@ -18,45 +18,19 @@ class KeyboardControl:
         self.key_strokes = dict()
 
         # Publishers
-        # Arrows Set Keyboard (+ rePage & avPage)
-        pub = rospy.Publisher("/command/1", Float32, queue_size = 1)
-        self.key_strokes["UP"] = {"pub": pub, "down": False, "up": True, "cmd": 0, "key_code": pygame.K_UP}
+        pub = rospy.Publisher("/command/2", Float32, queue_size = 1)
+        self.key_strokes["thrust"] = {"pub": pub, "down": [False, False], "up": [True, True], "cmd": [0, 0], "key_codes": [pygame.K_LSHIFT, pygame.K_LCTRL]}
 
-        pub = rospy.Publisher("/command/2", Float32, queue_size = 1)        
-        self.key_strokes["DOWN"] = {"pub": pub, "down": False, "up": True, "cmd": 0, "key_code": pygame.K_DOWN}
-
-        pub = rospy.Publisher("/command/3", Float32, queue_size = 1)
-        self.key_strokes["LEFT"] = {"pub": pub, "down": False, "up": True, "cmd": 0, "key_code": pygame.K_LEFT}
-        
-        pub = rospy.Publisher("/command/4", Float32, queue_size = 1)
-        self.key_strokes["RIGHT"] = {"pub": pub, "down": False, "up": True, "cmd": 0, "key_code": pygame.K_RIGHT}
-        
-        pub = rospy.Publisher("/command/5", Float32, queue_size = 1)
-        self.key_strokes["REPAG"] = {"pub": pub, "down": False, "up": True, "cmd": 0, "key_code": pygame.K_PAGEUP}
+        pub = rospy.Publisher("/command/1", Float32, queue_size = 1)        
+        self.key_strokes["roll"] = {"pub": pub, "down": [False, False], "up": [True, True], "cmd": [0, 0], "key_codes": [pygame.K_d, pygame.K_a]}
 
         pub = rospy.Publisher("/command/6", Float32, queue_size = 1)
-        self.key_strokes["AVPAG"] = {"pub": pub, "down": False, "up": True, "cmd": 0, "key_code": pygame.K_PAGEDOWN}
+        self.key_strokes["pitch"] = {"pub": pub, "down": [False, False], "up": [True, True], "cmd": [0, 0], "key_codes": [pygame.K_w, pygame.K_s]}
 
-        # Letters Set Keyboard (ASDW + EQ)
-        pub = rospy.Publisher("/command/7", Float32, queue_size = 1)
-        self.key_strokes["A"] = {"pub": pub, "down": False, "up": True, "cmd": 0, "key_code": pygame.K_a}
+        pub = rospy.Publisher("/command/4", Float32, queue_size = 1)
+        self.key_strokes["yaw"] = {"pub": pub, "down": [False, False], "up": [True, True], "cmd": [0, 0], "key_codes": [pygame.K_q, pygame.K_e]}
 
-        pub = rospy.Publisher("/command/8", Float32, queue_size = 1)
-        self.key_strokes["S"] = {"pub": pub, "down": False, "up": True, "cmd": 0, "key_code": pygame.K_s}
 
-        pub = rospy.Publisher("/command/9", Float32, queue_size = 1)
-        self.key_strokes["D"] = {"pub": pub, "down": False, "up": True, "cmd": 0, "key_code": pygame.K_d}
-
-        pub = rospy.Publisher("/command/10", Float32, queue_size = 1)
-        self.key_strokes["W"] = {"pub": pub, "down": False, "up": True, "cmd": 0, "key_code": pygame.K_w}
-
-        pub = rospy.Publisher("/command/11", Float32, queue_size = 1)
-        self.key_strokes["E"] = {"pub": pub, "down": False, "up": True, "cmd": 0, "key_code": pygame.K_e}
-
-        pub = rospy.Publisher("/command/12", Float32, queue_size = 1)
-        self.key_strokes["Q"] = {"pub": pub, "down": False, "up": True, "cmd": 0, "key_code": pygame.K_q}
-
-        
     def read_keyboard (self):
 
         # Update Key States (UP | DOWN)
@@ -64,27 +38,30 @@ class KeyboardControl:
 
             if event.type == pygame.KEYDOWN:
                 for key in self.key_strokes.values():
-                    if key["key_code"] == event.key:
-                        key["down"] = True
-                        key["up"] = False
+                    for i in range(2):
+                        if key["key_codes"][i] == event.key:
+                            key["down"][i] = True
+                            key["up"][i] = False
 
             elif event.type == pygame.KEYUP:
                 for key in self.key_strokes.values():
-                    if key["key_code"] == event.key:
-                        key["down"] = False
-                        key["up"] = True
+                    for i in range(2):
+                        if key["key_codes"][i] == event.key:
+                            key["down"][i] = False
+                            key["up"][i] = True
 
         # Update Command Values
         for key in self.key_strokes.values():
 
-            if key["down"]:
-                if key["cmd"] < abs(self.saturation - self.change_rate):
-                    key["cmd"] += self.change_rate
-                else:
-                    key["cmd"] = self.saturation
+            for i in range(2):
+                if key["down"][i]:
+                    if key["cmd"][i] < abs(self.saturation - self.change_rate):
+                        key["cmd"][i] += self.change_rate
+                    else:
+                        key["cmd"][i] = self.saturation
 
-            elif key["up"]:
-                key["cmd"] = 0
+                elif key["up"][i]:
+                    key["cmd"][i] = 0
 
 
     def run (self):
@@ -96,7 +73,7 @@ class KeyboardControl:
     def publish_commands(self):
 
         for key in self.key_strokes.values():
-            key["pub"].publish(key["cmd"])
+            key["pub"].publish(key["cmd"][0] - key["cmd"][1])
 
 
 if __name__ == "__main__":
